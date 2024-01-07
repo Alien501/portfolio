e7cab6d643a3b6416d2d720530496c05ee4f10d9
Alien501 <cvignesh404@gmail.com>
2024-01-07 08:09:41 +0530

    loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import LoadingScreen from './pages/LoadingScreen'
 
 function App() {
+  const [stars, setStars] = useState([]);
+    
+  useEffect(() => {
+      function addStars() {
+        const newStars = [];
+  
+        for (let index = 0; index < 75; index++) {
+          const x = Math.random() * (window.innerWidth - 10);
+          const y = Math.random() * (window.innerHeight - 10);
+          const size = Math.floor(Math.random()*6);
+          const z = Math.random([-1, 1, 2, 10, 6]);
+          const star = (
+            <div
+              key={index}
+              className={`star star${index}`}
+              style={{ transform: `translate(${x}px, ${y}px)`, width: `${size}px`, height: `${size}px`, zIndex: z}}
+            ></div>
+          );
+  
+          newStars.push(star);
+        }
+  
+        setStars(newStars);
+      }
+  
+      addStars();
+    }, []);
 
   return (
     <>
+      <LoadingScreen stars={stars}/>
     </>
   )
 }
---
+++ src/pages/LoadingScreen.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useLayoutEffect, useState } from "react";
+
+import RokcetImg from '../assets/rocket.svg'
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import './LoadingScreen.css'
+
+export default function LoadingScreen({stars}) {
+    const container = useRef();
+
+    useGSAP(() => {
+        gsap.fromTo('.rocket', {
+            bottom: '-20%',
+        }, {
+            transform: 'translate3d(0, -50vh, 0)',
+            duration: 4,
+            repeat: 0,
+        })
+    }, {scope: container})
+
+    useGSAP(() => {
+        gsap.to('.rocket',{
+            rotateZ: '0.5deg',
+            duration: .1,
+            rotationZ: '-0.5deg',
+            yoyo: true,
+            repeat: -1
+        })
+    }, {scope: container})
+
+    
+    useLayoutEffect(() => {
+        const contexts = [];
+
+        setTimeout(
+            () => {
+                for (let i = 0; i < stars.length; i++) {
+                    let ctx = gsap.context(
+                        () => {
+                            gsap.fromTo(`.star${i}`,
+                            {
+
+                            }
+                            ,{
+                                opacity: 0.9,
+                                duration: 0.8,
+                                opacity: 0.3,
+                                delay: Math.random()*2,
+                                yoyo: true,
+                                repeat: -1
+                            });
+                        });
+                    contexts.push(ctx)
+                };
+            }, 1000);
+
+            return () => {
+                contexts.forEach((ctx) => ctx.revert());
+            }
+
+    })
+
+    useLayoutEffect(() => {
+        const contexts = [];
+        setTimeout(() => {
+            for (let i = 0; i < stars.length; i++) {
+                const x = Math.random() * (window.innerWidth - 10);
+                const y = -Math.random() * window.innerHeight;
+                gsap.set(`.star${i}`, { x, y });
+    
+                let ctx = gsap.context(() => {
+                    gsap.fromTo(`.star${i}`,
+                    {
+
+                    }
+                    , {
+                        y: window.innerHeight + 10,
+                        duration: 15,
+                        delay: Math.random() * 35,
+                        ease: 'none',
+                        onComplete: () => {
+                            gsap.set(`.star${i}`, { y: -10 });
+                        },
+                        repeat: -1,
+                    });
+                });
+                contexts.push(ctx);
+            }
+        }, 1200);
+        return () => {
+            contexts.forEach((ctx) => ctx.revert());
+        };
+    });
+    
+
+    return(
+        <div ref={container} className="loading-page-container">
+            <img src={RokcetImg} alt="Rocket" className="rocket" />
+            {stars}
+        </div>
+    )
+}
